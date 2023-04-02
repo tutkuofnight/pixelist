5d5b9536506e669598061b64927b4a72dbf9e7fb
Tutku Ucan <72797914+tutkuofnight@users.noreply.github.com>
2023-04-02 17:10:46 +0300

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script>
+import {mapState} from 'vuex'
+export default {
+  name: 'IndexPage',
+  computed: {
+    ...mapState(['text'])
+  }
+}
+</script>
+
+<template lang="pug">
+#app
+  h1 {{text}}
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ store/index.js
@@ -0,0 +1,19 @@
+const state = () => ({
+  text: "Hi 🍃"
+})
+
+const getters = {
+  
+}
+
+const mutations = {
+
+}
+
+const actions = {
+
+}
+
+export default {
+  state , actions , getters , mutations
+}
